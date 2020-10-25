6e9e0b72a2bc3dea5079d15facf3fc3406c7bbf6
Yasi1995 <yasasi888@gmail.com>
2020-10-25 09:39:48 +0530

    first commit

---
+++ Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        fun funt = new fun();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            funt.LoadOriginalImage();
+            pictureBox1.ImageLocation = "1.jpg";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            funt.sobel();
+            pictureBox2.ImageLocation = "sobelx.jpg";
+            pictureBox3.ImageLocation = "sobely.jpg";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            funt.laplationwithoutnoice();
+            pictureBox4.ImageLocation = "lap2.jpg";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+           funt.laplationwithoutnoice();
+            pictureBox5.ImageLocation = "lap2.jpg";
+        }
+    }
+}
---
+++ fun.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenCvSharp;
+
+namespace WindowsFormsApp1
+{
+    class fun
+    {
+        IplImage src, gry, sobelX, sobely, tempx, tempy, lap, temp;
+
+
+        public void LoadOriginalImage()
+        {
+            src = Cv.LoadImage("building.jpg", LoadMode.Color);
+            Cv.SaveImage("1.jpg", src);
+        }
+
+        public void cnvertGray()
+        {
+
+            gry = Cv.CreateImage(src.Size, BitDepth.U8, 1);
+            Cv.CvtColor(src, gry, ColorConversion.RgbToGray);
+            Cv.SaveImage("2.jpg", gry);
+
+        }
+        public void sobel() // detect edges
+        {
+            cnvertGray();
+            sobelX = Cv.CreateImage(src.Size, BitDepth.U8, 1);
+            sobely = Cv.CreateImage(src.Size, BitDepth.U8, 1);
+
+            // create temp images
+            tempx = Cv.CreateImage(src.Size, BitDepth.S16, 1);
+            tempy = Cv.CreateImage(src.Size, BitDepth.S16, 1);
+
+            Cv.Sobel(gry, tempx, 1, 0, ApertureSize.Size3);
+            Cv.Sobel(gry, tempy, 0, 1, ApertureSize.Size3);
+
+            // convert singed 16 to unsinged 8
+
+            Cv.ConvertScaleAbs(tempx, sobelX);
+            Cv.ConvertScaleAbs(tempy, sobely);
+
+            //save
+
+            Cv.SaveImage("sobelx.jpg", sobelX);
+            Cv.SaveImage("sobely.jpg", sobely);
+        }
+        public void laplation() // detect edges
+        {
+            cnvertGray();
+            lap = Cv.CreateImage(src.Size, BitDepth.U8, 1);
+
+
+            // create temp images
+            temp = Cv.CreateImage(src.Size, BitDepth.S16, 1);
+
+
+            Cv.Laplace(gry, temp, ApertureSize.Size3);
+
+
+            // convert singed 16 to unsinged 8
+
+            Cv.ConvertScaleAbs(temp, lap);
+
+
+            //save
+
+            Cv.SaveImage("lap.jpg", lap);
+
+        }
+        public void laplationwithoutnoice() // detect edges
+        {
+            cnvertGray();
+            Cv.Smooth(gry, gry, SmoothType.Gaussian);
+            lap = Cv.CreateImage(src.Size, BitDepth.U8, 1);
+
+
+            // create temp images
+            temp = Cv.CreateImage(src.Size, BitDepth.S16, 1);
+
+
+            Cv.Laplace(gry, temp, ApertureSize.Size3);
+
+
+            // convert singed 16 to unsinged 8
+
+            Cv.ConvertScaleAbs(temp, lap);
+
+
+            //save
+
+            Cv.SaveImage("lap2.jpg", lap);
+
+        }
+
+
+
+
+    }
+}
